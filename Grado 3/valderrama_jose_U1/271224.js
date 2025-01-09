6dd0df18d97ec496dc75e2d5721cd14b0b373234
valderrama20 <jose23122009@gmail.com>
2025-01-09 13:56:27 +0100

    lenguaje de marcas, html-xml

---
+++ Grado 3/valderrama_jose_U1/271224.js
@@ -0,0 +1,89 @@
+
+// introduccion al diseño web
+
+/*  
+- definicion
+   - Planificacion
+   - Creacion
+   - Mantenimiento de sitios Web
+
+- Evolucion
+   - 1990s: HTML basico, diseños estaticos
+   - 2000s: CSS, Flash.
+   - 2010s: Diseño adaptativo, UX
+   - Actualidad: CSS Grid, Flexbox, accesibilidad
+*/
+
+// Diseño Grafico vs Diseño Web
+
+/* 
+- Diseño Grafico
+   - Enfocado en medios impresos
+   - Herramientas: Photoshop, Illustrator
+
+- Diseño Web
+   - Interfaces digitales interactivas
+   - Uso de HTML, CSS y JavaScript
+   - Adaptabilidad y funcionalidad 
+*/
+
+// Principios del diseño Web
+
+/* 
+- Usabilidad:
+   - Navegacion intuitiva, velocidad de carga
+
+- Accesibilidad: 
+   - Inclusion de usuarios con discapacidades
+   
+- UX:
+   - Experiencia general del usuario
+
+- Diseño Adaptativo:
+   - Ajuste a dispositivos diversos
+*/
+
+// Proceso de diseño web
+
+/*
+- Investigacion:
+   - Publico objectivo, competidores
+
+- Planificacion:
+   -Sitemap, flujo de navegacion.
+
+- Wireframes: 
+   - Diseño estructural basico.
+
+- Prototipos: 
+   - Versiones interactivas para pruebas.
+*/
+
+// Expericencia del usuario
+
+/*
+
+-Diseño UX:
+   - La expereinecia del usuario abarca todos
+   los aspectos de la interaccion del usuario final con 
+   la empresa, sus servicios y sus productos
+
+-Diseño UI: ^
+   - UI como una practica puramente digital, que tienene en cuenta todo
+ */
+
+
+   /* 
+-----------------------------------------------------
+        -    Amazon         -
+        -                   -
+        -                   -
+        -
+        -
+        -
+        -
+        -
+----------------------------------------------------
+
+
+*/
